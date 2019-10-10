649ae4e7b22d182b2bbe541b01941de838b88ec6
zhanggl <zhanggl@eternalinfo.cn>
2019-10-10 23:14:28 +0800

    linked add

---
+++ recommender/DataCommnd/src/main/java/cn/eternal/com/lettcode/linked/LinkedUtils.java
@@ -0,0 +1,21 @@
+package cn.eternal.com.lettcode.linked;
+
+import cn.eternal.com.lettcode.Node;
+
+/**
+ * @program: moviceSystem
+ * @description:
+ * @author: zgl
+ * @create: 2019-10-10 22:08
+ **/
+public class LinkedUtils {
+    Node head = null;
+
+    /**
+     * add new node to Linked
+     * @param data
+     */
+    public void addNode(int data){
+
+    }
+}
